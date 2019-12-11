1c345532d81ee2497c571eb694f241450a366686
chetan rawat <chetan.rawat@hicom.in>
2019-12-11 11:38:47 +0530

    Initial Commit

---
+++ DemoOfLabelRenderer/DemoOfLabelRenderer.Android/CustomLabelRenderer.cs
@@ -0,0 +1,20 @@
+﻿using Android.Content;
+using DemoOfLabelRenderer.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace DemoOfLabelRenderer.Droid
+{
+    class CustomLabelRenderer : LabelRenderer
+    {
+        public CustomLabelRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetTextIsSelectable(true);
+        }
+    }
+}
---
+++ DemoOfLabelRenderer/DemoOfLabelRenderer.iOS/CustomLabelRenderer.cs
@@ -0,0 +1,70 @@
+﻿using DemoOfLabelRenderer.iOS;
+using Foundation;
+using System;
+using System.ComponentModel;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Label), typeof(CustomLabelRenderer))]
+namespace DemoOfLabelRenderer.iOS
+{
+    public class CustomLabelRenderer : ViewRenderer<Label, UITextView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            if (e.NewElement != null)
+            {
+                if (Control == null)
+                {
+                    SetNativeControl(new UITextView());
+                }
+
+                UpdateText();
+            }
+
+            base.OnElementChanged(e);
+        }
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (e.PropertyName == nameof(Label.Text))
+            {
+                UpdateText();
+            }
+        }
+        void UpdateText()
+        {
+            if (string.IsNullOrWhiteSpace(Element?.Text))
+            {
+                Control.Text = string.Empty;
+                return;
+            }
+
+            NSError error = null;
+            Control.AttributedText = new NSAttributedString(NSData.FromString(Element.Text),
+                                                            new NSAttributedStringDocumentAttributes { DocumentType = NSDocumentType.PlainText },
+                                                            ref error);
+
+            switch (Element.FontAttributes)
+            {
+                case FontAttributes.None:
+                    Control.Font = UIFont.SystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+                case FontAttributes.Bold:
+                    Control.Font = UIFont.BoldSystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+                case FontAttributes.Italic:
+                    Control.Font = UIFont.ItalicSystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+                default:
+                    Control.Font = UIFont.BoldSystemFontOfSize(new nfloat(Element.FontSize));
+                    break;
+            }
+            Control.Selectable = true;
+            Control.Editable = false;
+            Control.ScrollEnabled = false;
+            Control.ShouldInteractWithUrl += delegate { return true; };
+        }
+    }
+}
